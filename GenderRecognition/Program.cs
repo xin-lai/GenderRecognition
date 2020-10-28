f321687c3ed84c1b6fc027821b49075733eb78d0
wenqiang.li <codelove1314@live.cn>
2020-10-28 11:25:35 +0800

    添加项目文件。

---
+++ GenderRecognition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenderRecognition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
